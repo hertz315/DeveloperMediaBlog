23262a9ce3b968a7b1137c926fa26b969f0bc3a5
hertz315 <hertz315@gmail.com>
2022-12-17 22:54:42 +0900

    폴더 구조 정리, 탭바 색상 변경

--- DeveloperMediaBlog/Core/Main/View/MainView.swift
+++ DeveloperMediaBlog/Core/Main/View/MainView.swift
@@ -12,22 +12,29 @@ struct MainView: View {
     
     var body: some View {
         // MARK: - 탭뷰 (포스트/프로필)
-        
         TabView {
             Text("피드")
                 .tabItem {
                     Image(systemName: "rectangle.portrait.on.rectangle.portrait.angled")
                     Text("Post's")
                 }
+                .toolbar(.visible, for: .tabBar)
+                .toolbarBackground(
+                    Color.gray.opacity(0.05),
+                    for: .tabBar)
             
             ProfileView()
                 .tabItem {
                     Image(systemName: "gear")
                     Text("Profile")
                 }
+                .toolbar(.visible, for: .tabBar)
+                .toolbarBackground(
+                    Color.gray.opacity(0.05),
+                    for: .tabBar)
             
         }
-        .tint(.black)
+        .accentColor(.black)
         
     }
 }
